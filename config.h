69608952378bfc94685808a523b3736dda254da7
ryukamish <10929626-ryukamish@users.noreply.github.com>
2022-04-25 18:01:22 +0530

    font fix

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono Medium:size=11"
+	"mon:style=Bold:size=14",
+    "Noto Color Emoji:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
