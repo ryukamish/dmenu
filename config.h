965f40062c69872706f212d569766091e8c6a30f
ryukamish <10929626-ryukamish@users.noreply.github.com>
2022-04-28 11:44:06 +0530

    dmenu will work even without pywal

--- config.h
+++ config.h
@@ -4,16 +4,25 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mono:style=Bold:size=14",
+	"mono:style=Bold:size=13",
     "Noto Color Emoji:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-// The below line adds colors to dmenu with Pywal generated header file
-// Dependencies:
-//             pywal
-// Pywal colorschemes are put in the .cache directory in the $HOME directory
-#include "/home/kunal/.cache/wal/colors-wal-dmenu.h"
+/* Dmenu will still compile without pywal */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#dfe2ea", "#0c0812" },
+	[SchemeSel] = { "#dfe2ea", "#9F718C" },
+	[SchemeOut] = { "#dfe2ea", "#B7C7DE" },
+};
+
+/* The below line adds colors to dmenu with Pywal generated header file
+* Dependencies:
+*             pywal
+* Pywal colorschemes are put in the .cache directory in the $HOME directory
+* #include "/home/kunal/.cache/wal/colors-wal-dmenu.h"
+*/
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
